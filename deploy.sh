f0dfd80895cbe39deeaca4bb4f810c352fd3f654
zaidanm16 <zaidanmuhammad169@gmail.com>
2023-07-05 10:39:06 +0700

    Version 1

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo 'widan16' | docker secret create mysql_root_password -
-docker stack deploy -c widan.yml widan --with-registry-auth
--- user.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Add User to docker group
-usermod -aG docker $USER
-
-# Give permission
-chmod 666 /var/run/docker.sock
